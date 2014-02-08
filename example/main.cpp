60a0a370fed3474921e1ba2c3f767bfd604de491
Ryan Norby <norby2@illinois.edu>
2014-02-08 02:00:31 -0600

    Made whitespace consistent.

--- example/main.cpp
+++ example/main.cpp
@@ -13,30 +13,30 @@ void drawShapes(sf::RenderWindow& window);
 
 // Program entry point
 int main() {
-    // Create the SFML window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Crane Game!");
+	// Create the SFML window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Crane Game!");
 
-    sf::Event event;	// For polling events that SFML sends us
-    sf::Clock clock;	// For keeping track of time
+	sf::Event event;	// For polling events that SFML sends us
+	sf::Clock clock;	// For keeping track of time
 
-    while (window.isOpen()) {
+	while (window.isOpen()) {
 		// Process any events sent by SFML since the last frame
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
 				// SFML sent us a close event, so clean up
 				window.close();
-            }
-        }
+			}
+		}
 
 		// Clear the window before we start drawing to it
 		window.clear();
-		
+
 		// Render some shapes
 		drawShapes(window);
-		
+
 		// Notify the window that we're ready to render
 		window.display();
-    }
+	}
 	return 0;
 }
 
